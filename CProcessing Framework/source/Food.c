94eaac733221770ef873ce74de5859978e61623e
xxuanxx <lilacxxuanxx@gmail.com>
2020-10-21 14:38:35 +0800

    food.c and change apple picture

--- CProcessing Framework/source/Food.c
+++ CProcessing Framework/source/Food.c
@@ -20,28 +20,9 @@ void foodSpawn(struct SnakeBody* sb)
 	foodPos.x = (float)((int)((CP_Random_GetInt() % ScreenWidth) - (ScreenWidth / 2))) * (sb->bodyWidth);
 	foodPos.y = (float)((int)((CP_Random_GetInt() % ScreenHeight) - (ScreenHeight / 2))) * (sb->bodyHeight);
 	
-	/*if (foodPos.x > WINDOW_WIDTH)
-	{
-		checkMoreX = 1;
-		while(checkMoreX == 1)
-		{
-			foodPos.x = (float)((int)CP_Random_GetInt() % ScreenWidth - (ScreenWidth / 2)) * (sb->bodyWidth);
-		}
-	}
-	
-	if (foodPos.y > WINDOW_HEIGHT)
-	{
-		checkMoreY = 1;
-		if (checkMoreY == 1)
-		{
-			foodPos.y = (float)((int)CP_Random_GetInt() % ScreenHeight - (ScreenHeight / 2)) * (sb->bodyHeight);
-		}
-	}
-	*/
 	//foodPos.x = (float)((int)CP_Random_GetFloat() %24 - 12)*(sb -> bodyWidth); 
 	//1200/50 = 24;  %24 is to get grid coords; -12 is to get pos and neg
-	//foodPos.y = (float)((int)CP_Random_GetInt() % ScreenHeight - (ScreenHeight / 2)) * (sb->bodyHeight);
-	//foodPos.x = (float)((int)CP_Random_GetInt() % ScreenWidth - (ScreenWidth / 2)) * (sb->bodyWidth);
+	
 }
 
 void FoodCollision_update(struct SnakeBody* sb)
